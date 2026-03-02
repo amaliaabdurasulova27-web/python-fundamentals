913af6a4811afdea5e7c3f2dc70b098d6afade01
Amalia <amaliaabdurasulova27@gmail.com>
2026-03-02 18:42:10 +0500

    Day one of practing python base, solved tasks on variables and input

---
+++ day1_task.py
@@ -0,0 +1,120 @@
+# result = (23 + 27) * 12
+# print(result)
+#
+#
+#
+# favourite_number = 7
+# print(f'My fav num {favourite_number}')
+#
+#
+#
+#
+# a = 10
+# b = 5
+# temp = a
+# a = b
+# b = temp
+# print(a, b)
+#
+#
+#
+# x = 42
+# y = 3.14
+# z = "Python"
+# flag = True
+# print(type(x))
+# print(type(y))
+# print(type(z))
+# print(type(flag))
+
+
+
+# s = '123'
+# num = int(s)
+# print(num * 2)
+
+
+# length = int(input('Введите длинну:'))
+# width = int(input('Ведите ширину:'))
+# area = length + width
+# print(area)
+
+
+# age = int(input('Ведите свой год рождения: '))
+# print(f'в 2030 году вам будет: {2030 - age}')
+
+
+
+# num1 = int(input("Ведите первое число: "))
+#
+# num2 = int(input("Ведите второе число: "))
+#
+# num3 = int(input("Ведите третье число: "))
+#
+# print(f'Среднее арифметическое: { (num1 + num2+ num3) / 3}')
+
+# переменные +input
+
+#задача номер 1
+
+# дано 3 переменные, имя, возраст и город
+# цель-вывести
+#
+# имя = имя человека
+#
+# возраст = сколько лет
+#
+# город = название города
+#
+# name = 'Amaliya'
+#
+# age = 16
+#
+# city = 'Tashkent'
+#
+# print(f'my name is {name} and my age is {age} and my city is {city}')
+
+
+# задача номер 2
+
+# задача - вывести число возведенное в степень
+#
+# псевдокод:
+# получить число
+# вывести полученное число возведенное в квадрат
+
+# num = int(input("Enter a number: "))
+#
+# if num == 0 :
+#     print(num, "is zero")
+# else:
+#     print(num ** 2)
+
+# задача номер 3
+#
+# задача - ввести два числа и вывести сумму
+
+# num1 = int(input("Enter number1: "))
+#
+# num2 = int(input("Enter number2: "))
+#
+# print(num1 + num2)
+
+# задача  4
+# задача - преобразовать строку '50' в число и прибавить 20
+
+# преобразовать строку 50 в число
+# прибавить 20
+
+# num = '50'
+# print(50 + int(num))
+
+# box = 13
+# box1 = 'angela'
+# box2 = True
+#
+# print(type(box))
+# print(type(box1))
+# print(type(box2))
+
+
